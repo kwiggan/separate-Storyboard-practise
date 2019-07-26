00cca1b2632e399200c5fb3ce84f7f9da593a99d
kwiggan <40346781+kwiggan@users.noreply.github.com>
2019-07-26 09:58:52 -0700

    Add files via upload
    create a practice app to have separate view controllers connected from multiple storyboards

---
+++ separateStoryboardsPractise/separateStoryboardsPractise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  separateStoryboardsPractise
+//
+//  Created by Keneisha Wiggan on 7/26/19.
+//  Copyright © 2019 Keneisha Wiggan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
